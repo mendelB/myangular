ea1046806eefac65d58cb476c20f02ec328d4b7e
mendelB <mendelblesofsky@gmail.com>
2016-04-04 17:56:08 -0400

    up to giving up on an unstable digest'

---
+++ src/scope.js
@@ -0,0 +1,45 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function initWatchVal() { }
+
+function Scope() {
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listnerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listnerFn: listnerFn || function() {},
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$$digestOnce = function() {
+  var self = this;
+  var newValue, oldValue, dirty;
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listnerFn(newValue,
+        (oldValue === initWatchVal ? newValue : oldValue),
+        self);
+      dirty = true;
+    }
+  });
+  return dirty;
+};
+
+Scope.prototype.$digest = function() {
+  var ttl = 10;
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw "10 digest iterations reached";
+    }
+  } while (dirty);
+};
